688ea6c1dd14ce7616c99d8333e8a24681430b89
hana87hana87 <86666260+hana87hana87@users.noreply.github.com>
2021-08-22 22:02:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 $(function(){
 
-    $('.date').click(function(){
-        var $news = $(this).find('.news');
-        if($news.hasClass('open')){
-        $news.removeClass('open');
-      $news.slideUp();
-      $(this).find('span').text('+');
-        } else{
-            $news.addClass('open');
-      $news.slideDown();
-      $(this).find('span').text('-');
-
-        }
+    $(function(){
+        $('.news_item').click(function(){
+            var news = $(this).find('.news');
+            if(news.hasClass('open')){
+                news.removeClass('open');
+                news.slideUp();
+                $(this).find('span').text('+');
+            } else {
+                news.addClass('open');
+                news.slideDown();
+                $(this).find('span').text('-');
+            }
+        });
     });
 
     
