48b2886912fa8eb0476f61551935374974975b87
hana87hana87 <86666260+hana87hana87@users.noreply.github.com>
2021-08-22 15:05:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    $('.date').click(function(){
+        var $news = $(this).find('.news');
+        if($news.hasClass('open')){
+        $news.removeClass('open');
+      $news.slideUp();
+      $(this).find('span').text('+');
+        } else{
+            $news.addClass('open');
+      $news.slideDown();
+      $(this).find('span').text('-');
+
+        }
+    });
+
+    
+
+
+
+
+
+
+});
